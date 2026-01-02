9b0415b1f8ba09519bda14ed549edf0fc2a987ab
mdsadrealam45 <mdsadrealam8207@gmail.com>
2026-01-03 04:11:37 +0530

    middleware, jobController and jobRoutes are added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
+const jobRoutes = require('./routes/jobRoutes');
+
 
 
 const app = express();
@@ -21,6 +23,8 @@ app.get('/', (req, res) => {
   res.send("Escrow API is Running...");
 });
 app.use('/api/users', userRoutes);
+app.use('/api/jobs', jobRoutes);
+
 
 // Start Server
 const PORT = process.env.PORT || 5000;
